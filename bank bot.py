4a4f59d65e779cb23f52d9086d7b76d53ae9220c
code union <55399768+commonwealth-codeunion@users.noreply.github.com>
2019-11-21 22:46:28 +0600

    Create bank bot.py

---
+++ bank bot.py
@@ -0,0 +1,51 @@
+import telebot
+from telebot import types
+import requests
+
+linkval = 'https://openexchangerates.org/api/latest.json?app_id=60da2bd9b3064714b2c5f2e8b00fbd40'
+
+data = requests.get(linkval)
+slovar = data.json()
+valuty = slovar['rates']
+
+
+token = '1061153932:AAFPiMBR-bpTtqAHkz_dA4sTvsS-ktgUWQ4'
+
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start'])
+def send_message(msg):
+    name = msg.chat.first_name
+    cid = msg.chat.id
+    bot.send_message(chat_id=cid, text='Hello, '+name, reply_markup=create_keyboard(menu))
+
+def create_keyboard(words=None, width=1, isOneTime=False, isPhone=False):
+    keyboard = types.ReplyKeyboardMarkup(one_time_keyboard=isOneTime, row_width=width, resize_keyboard = True)
+    for word in words:
+        keyboard.add(types.KeyboardButton(text=word, request_contact=isPhone))
+    return keyboard
+
+menu = ['Adress', 'Contacts', 'manuu']
+
+@bot.message_handler(content_types=['text'])
+def send_message(msg):
+    cid = msg.chat.id
+    content = msg.text
+    if content == 'Adress':
+        bot.send_location(chat_id=cid, latitude=43.219141, longitude=76.846924)
+    elif content == 'Contacts':
+        bot.send_message(chat_id=cid, text='+7 800 080-18-80')
+    elif content == 'manuu':
+        bot.send_message(chat_id=cid, text='Hello world3')
+
+
+
+
+
+
+
+bot.polling(none_stop=True)
+
+
+
+#связь со строительными кампаниями,видеть квартиры тд
